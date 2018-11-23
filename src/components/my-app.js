4a0bba4b5d370bd4a903c47ea401d41f51e3c196
Peter Halliday <pghalliday@gmail.com>
2018-11-23 16:53:36 +0100

    add a new view

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -50,8 +50,8 @@ class MyApp extends connect(store)(LitElement) {
         --app-section-even-color: #f7f7f7;
         --app-section-odd-color: white;
 
-        --app-header-background-color: white;
-        --app-header-text-color: var(--app-dark-text-color);
+        --app-header-background-color: lavender;
+        --app-header-text-color: var(--app-light-text-color);
         --app-header-selected-color: var(--app-primary-color);
 
         --app-drawer-background-color: var(--app-secondary-color);
@@ -191,6 +191,7 @@ class MyApp extends connect(store)(LitElement) {
         <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
         <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
         <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
+        <a ?selected="${this._page === 'view4'}" href="/view4">New View!</a>
       </nav>
     </app-header>
 
@@ -201,6 +202,7 @@ class MyApp extends connect(store)(LitElement) {
         <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
         <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
         <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
+        <a ?selected="${this._page === 'view4'}" href="/view4">New View!</a>
       </nav>
     </app-drawer>
 
@@ -209,6 +211,7 @@ class MyApp extends connect(store)(LitElement) {
       <my-view1 class="page" ?active="${this._page === 'view1'}"></my-view1>
       <my-view2 class="page" ?active="${this._page === 'view2'}"></my-view2>
       <my-view3 class="page" ?active="${this._page === 'view3'}"></my-view3>
+      <my-view4 class="page" ?active="${this._page === 'view4'}"></my-view4>
       <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
     </main>
 
