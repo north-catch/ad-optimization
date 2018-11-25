42f54219ed34360e0290afe953ec520c5eab1dd6
Peter Halliday <pghalliday@gmail.com>
2018-11-26 00:00:19 +0100

    gah, firebase imports break polymer buildgit add -Agit add -Agit add -A

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
 import * as firebaseui from 'firebaseui';
 
 export const app = firebase.initializeApp({
